a592e06a0651ba7b1c1592cede81e5c5ac389e7f
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-16 23:59:01 +1000

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <PortfolioContainer />
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <footer className="footer">
+        <h1>Mohit Arora</h1>
+      </footer>
+    );
+  }
+  
+  export default Header;
