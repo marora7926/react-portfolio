447614d737e521b48f6c245b2a7f6c725f951e1c
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-16 23:18:53 +1000

    dummy codes and app working

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
-export default function Home() {
+export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+        <h2>Portfolio</h2>
+        <p>
+            My name is Mohit, born in New Delhi, national capital of an amazing country, India.
+            I have a graduate degree in Physiotherapy and Diploma in Clinical Research from
+            India. In 2013, I received the prestigious 
+            Prime Minister Australia Asia Post Graduate Research Award to pursue 
+            Ph.D. in Medicine at the University of Sydney, Australia.
+        </p>
+        <p>
+            I am based in Sydney, NSW and have enrolled in the 24-weeks 
+            <i>"full stack flex coding bootcamp course"</i> at the University of Sydney 
+            to gain knowledge and develop skills to build dynamic end-to-end web applications
+            and become a full stack web developer. As a Postdoctoral Research Fellow
+            at the University of Sydney, I have been utilising this new knowledge and skills 
+            in developing research project websites and disseminating the research findings
+            to a broad range of stakeholders. In addition, I will continue to ultilise these
+            skills in my current jole role where I will be developing a digital solution for 
+            useful health-related educational resources for people with neurological disorders.  
+        </p>
+        <p>
+            I like drawing, eating spicy foods, learning international languages, cycling and running. 
+        </p>
     </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Blog() {
+export default function Resume() {
   return (
     <div>
-      <h1>Resume</h1>
+      <h2>Resume</h2>
       <p>
         Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
         Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
